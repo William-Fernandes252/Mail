98ee56981fe22790ca5c3d49be23afc08db7b002
William-Fernandes252 <william.winchester1967@gmail.com>
2022-02-20 15:56:25 -0300

    Move the script for generation of emails.

--- mail/utils.py
+++ mail/utils.py
@@ -1,11 +1,18 @@
-from mail.models import *
+from .models import *
 import random
 import string
 
-# Generates emails for test custom features
-def generateRandomEmails(n):
+
+def gen_emails(n: int) -> None:
+    """Generates random emails for testing purposes
+
+    Args:
+        n (int): Number of emails to generate.
+    """
+    
     user = User.objects.get(username='william@example.com')
     sender = User.objects.get(username='service@example.com')
+    
     for i in range(n):
         randomCode = ''.join(random.choices(string.digits, k = 6))
         subject = 'Your code is ' + randomCode
@@ -13,4 +20,3 @@ def generateRandomEmails(n):
         email.save()
         email.recipients.add(user)
         email.save()
-    return True
